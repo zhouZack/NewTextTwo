70bf9f9306b501f265ebf69f757ed74e1978d520
Josh.Shron <joshshron@ACAC0203.ipt.aol.com>
2015-10-29 17:04:43 +0800

    update the viewController with translate

--- OAProject/ViewController.m
+++ OAProject/ViewController.m
@@ -13,6 +13,8 @@
 //@property (nonatomic, strong) UIView *circle;
 @property (nonatomic, assign) CGPoint centerPoint;
 @property (nonatomic, strong) NSMutableArray *circleArray;
+@property (nonatomic, strong) NSMutableArray *backBtnArray;
+@property (nonatomic, strong) NSMutableArray *frontBtnArray;
 @property (nonatomic, strong) NSNumber *timer;
 
 @end
@@ -28,12 +30,19 @@
     NSArray *circleSize = @[NSStringFromCGSize(CGSizeMake(self.view.width / 4, self.view.width / 4)),NSStringFromCGSize(CGSizeMake(60, 60)),NSStringFromCGSize(CGSizeMake(60, 60)),NSStringFromCGSize(CGSizeMake(80, 80)),NSStringFromCGSize(CGSizeMake(80, 80))];
     CGPoint centerPoint = CGPointZero;
     _circleArray = [[NSMutableArray alloc] init];
+    _backBtnArray = [[NSMutableArray alloc] init];
+    _frontBtnArray = [[NSMutableArray alloc] init];
     for (int index = 0; index < circleSize.count;index++ ) {
+        
+        CGFloat widthFloat = self.view.width / 320;
+        CGFloat heightFloat = self.view.height / 480;
         CGSize size = CGSizeFromString(circleSize[index]);
-        UIView *circle = [[UIView alloc] init];
+        size.width *= widthFloat;
+        size.height = size.width;
+        UIButton *circle = [[UIButton alloc] init];
         circle.size = size;
         circle.centerX = self.view.width / 2;
-        circle.centerY = self.view.height / 2 - 100;
+        circle.centerY = self.view.height / 2 - 100 * heightFloat;
         circle.layer.cornerRadius = circle.width / 2;
         circle.tag = index;
         if (index != 0) {
@@ -49,30 +58,30 @@
                 break;
             case 1:
             {
-                circle.centerX = centerPoint.x - 60;
-                circle.centerY = centerPoint.y - 60;
+                circle.centerX = centerPoint.x - 60 * widthFloat;
+                circle.centerY = centerPoint.y - 60 * heightFloat;
                 circle.backgroundColor = [UIColor purpleColor];
                 break;
             }
             case 2:
             {
-                circle.centerX = centerPoint.x + 60;
-                circle.centerY = centerPoint.y - 90;
+                circle.centerX = centerPoint.x + 60 * widthFloat;
+                circle.centerY = centerPoint.y - 90 * heightFloat;
                 circle.backgroundColor = [UIColor blackColor];
                 break;
             }
             case 3:
             {
-                circle.centerX = centerPoint.x - 70;
-                circle.centerY = centerPoint.y + 80;
+                circle.centerX = centerPoint.x - 70 * widthFloat;
+                circle.centerY = centerPoint.y + 80 * heightFloat;
                 circle.backgroundColor = [UIColor yellowColor];
                 break;
             }
             case 4:
             {
                 
-                circle.centerX = centerPoint.x + 100;
-                circle.centerY = centerPoint.y + 100;
+                circle.centerX = centerPoint.x + 100 * widthFloat;
+                circle.centerY = centerPoint.y + 100 * heightFloat;
                 circle.backgroundColor = [UIColor orangeColor];
                 break;
             }
@@ -80,8 +89,17 @@
             default:
                 break;
         }
-        [self addanimationWithToPoint:circle.center view:circle];
-        [_circleArray addObject:circle];
+        [circle addTarget:self action:@selector(touchEvent:) forControlEvents:UIControlEventTouchUpInside];
+        UIButton *backView = [[UIButton alloc] init];
+        UIView *contentView = [self transfrontView:backView frontView:circle];
+        backView.backgroundColor = [UIColor blackColor];
+        [backView addTarget:self action:@selector(touchEvent:) forControlEvents:UIControlEventTouchUpInside];
+        [backView setTitle:@"敬请期待" forState:0];
+        backView.titleLabel.font = [UIFont systemFontOfSize:15];
+        [self addanimationWithToPoint:contentView.center view:contentView];
+        [_backBtnArray addObject:backView];
+        [_frontBtnArray addObject:circle];
+        [_circleArray addObject:contentView];
     }
     
     UIButton *jitterBtn = [[UIButton alloc] init];
@@ -115,8 +133,33 @@
     group.fillMode = kCAFillModeForwards;
     group.animations = @[animation,yidong,opacityAnimation];
     [view.layer addAnimation:group forKey:@"yidong"];
-    
-    
+}
+
+- (UIView *)transfrontView:(UIView *)backView frontView:(UIView *)frontView
+{
+    UIView *contentView = [[UIView alloc] initWithFrame:frontView.frame];
+    frontView.origin = CGPointZero;
+    backView.origin = CGPointZero;
+    backView.size = frontView.size;
+    backView.layer.cornerRadius = frontView.layer.cornerRadius;
+    backView.tag = (frontView.tag + 1) * 10000;
+    [contentView addSubview:frontView];
+    [contentView sendSubviewToBack:backView];
+    return contentView;
+}
+
+- (void)touchEvent:(UIButton *)sender
+{
+    NSInteger index = 0;
+    UIView *backView = nil;
+    if (sender.tag >= 10000) {
+        index = sender.tag / 10000 - 1;
+        backView = _frontBtnArray[index];
+    } else {
+        index = sender.tag;
+        backView = _backBtnArray[index];
+    }
+     [UIView transitionFromView:sender toView:backView duration:0.5f options:UIViewAnimationOptionTransitionFlipFromLeft completion:NULL];
 }
 
 - (void)jitterAnimation
